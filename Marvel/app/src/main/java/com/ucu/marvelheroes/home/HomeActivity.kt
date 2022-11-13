98f29633f382db053a8cd1d353383e7f36282f18
gcabrera243 <gcabrera243@gmail.com>
2022-11-13 20:34:24 -0300

    imports innecesarios

--- Marvel/app/src/main/java/com/ucu/marvelheroes/home/HomeActivity.kt
+++ Marvel/app/src/main/java/com/ucu/marvelheroes/home/HomeActivity.kt
@@ -1,18 +1,11 @@
 package com.ucu.marvelheroes.home
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
-import android.widget.EditText
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
+
 import com.ucu.marvelheroes.R
-import com.ucu.marvelheroes.data.domain.model.MarvelCharacter
-import java.util.*
+
 
 class HomeActivity : AppCompatActivity(){
 
